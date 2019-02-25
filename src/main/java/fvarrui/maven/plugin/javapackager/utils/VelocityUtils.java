3cb3a115b8c343821f99032e83b043d02d99c267
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2019-02-25 13:24:21 +0000

    U

--- src/main/java/fvarrui/maven/plugin/javapackager/utils/VelocityUtils.java
+++ src/main/java/fvarrui/maven/plugin/javapackager/utils/VelocityUtils.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
@@ -11,28 +12,34 @@ import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
 public class VelocityUtils {
-	
+
 	private static VelocityEngine velocityEngine;
-	
+
 	static {
 		velocityEngine = new VelocityEngine();
-		velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath"); 
+		velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
 		velocityEngine.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
 		velocityEngine.init();
 	}
-	
-	public static void render(String templatePath, File output, String prefix, Object data) throws IOException {
-		VelocityContext context = new VelocityContext();
-		context.put(prefix, data);
-	
-		Template template = velocityEngine.getTemplate(templatePath, "UTF-8");
-		
-		FileWriter fw = new FileWriter(output);
-		
-		template.merge(context, fw);
-		
-		fw.flush();
-		fw.close();
+
+	public static void render(String templatePath, File output, String prefix, Object data) throws MojoExecutionException {
+		try {
+
+			VelocityContext context = new VelocityContext();
+			context.put(prefix, data);
+
+			Template template = velocityEngine.getTemplate(templatePath, "UTF-8");
+
+			FileWriter fw = new FileWriter(output);
+
+			template.merge(context, fw);
+
+			fw.flush();
+			fw.close();
+
+		} catch (IOException e) {
+			throw new MojoExecutionException(e.getMessage(), e);
+		}
 	}
 
 }
