cecbfc7e1bb7d565b3dda3d7c3047a274ab57e95
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2019-02-26 02:20:21 +0000

    U generate jre with jlink

--- src/main/java/fvarrui/maven/plugin/javapackager/utils/ProcessUtils.java
+++ src/main/java/fvarrui/maven/plugin/javapackager/utils/ProcessUtils.java
@@ -10,30 +10,33 @@ import org.apache.maven.plugin.logging.Log;
 
 public class ProcessUtils {
 	
-	public static void exec(String... command) throws MojoExecutionException {
-		exec(null, command);
-	}
-
-	public static void exec(Log log, String... command) throws MojoExecutionException {
+	public static String exec(Log log, String... command) throws MojoExecutionException {
+		StringBuffer outputBuffer = new StringBuffer();
 		try { 
 			Process process = Runtime.getRuntime().exec(command);
-			if (log != null) {
-				log.info("Command line: " + StringUtils.join(command, " "));
-				BufferedReader output = new BufferedReader(new InputStreamReader(process.getInputStream()));
-				BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-				while (process.isAlive()) {
-					if (output.ready()) {
-						log.info(output.readLine());
-					}
-					if (error.ready()) {
-						log.info(error.readLine());
-					}
+			if (log != null) log.info("Command line: " + StringUtils.join(command, " "));
+			BufferedReader output = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+			while (process.isAlive()) {
+				if (output.ready()) {
+					String line = output.readLine();
+					if (log != null) log.info(line);
+					outputBuffer.append(line);
+				}
+				if (log != null && error.ready()) {
+					log.error(error.readLine());
 				}
 			}
-			process.waitFor();
-		} catch (IOException | InterruptedException e) {
+			output.close();
+			error.close();
+		} catch (IOException e) {
 			throw new MojoExecutionException(e.getMessage(), e);
 		}
+		return outputBuffer.toString();
+	}
+	
+	public static String exec(String... command) throws MojoExecutionException {
+		return exec(null, command);
 	}
 
 }
