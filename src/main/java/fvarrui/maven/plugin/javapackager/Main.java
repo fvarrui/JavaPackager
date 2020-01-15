5cf00a8892f8dffb001655b386ec8f710579c63a
Fran.vargas(ruiz) <4354486+fvarrui@users.noreply.github.com>
2020-01-15 14:25:28 +0000

    U command execution test

--- src/main/java/fvarrui/maven/plugin/javapackager/Main.java
+++ src/main/java/fvarrui/maven/plugin/javapackager/Main.java
@@ -1,5 +1,7 @@
 package fvarrui.maven.plugin.javapackager;
 
+import java.io.File;
+
 import org.apache.maven.plugin.MojoExecutionException;
 
 import fvarrui.maven.plugin.javapackager.utils.CommandUtils;
@@ -8,7 +10,8 @@ public class Main {
 
 	public static void main(String[] args) throws MojoExecutionException {
 		String result = 
-			CommandUtils.execute(
+			CommandUtils.execute2(
+				new File("."),
 				"/Library/Java/JavaVirtualMachines/adoptopenjdk-13.jdk/Contents/Home/bin/jdeps", 
 				"-q",
 				"--ignore-missing-deps",
@@ -17,6 +20,7 @@ public class Main {
 				"/Users/fran/teuton-panel/target/app/teuton-panel.app/Contents/Resources/Java/libs/*.jar",
 				"/Users/fran/teuton-panel/target/teuton-panel-0.1.1-runnable.jar"
 			);
+			
 		System.out.println(result);
 	}
 
