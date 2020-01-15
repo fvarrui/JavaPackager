e85176066bb19ed46d36f994f1ba669d164955b7
Fran.vargas(ruiz) <4354486+fvarrui@users.noreply.github.com>
2020-01-15 16:28:02 +0000

    U fixed bug determining modules from libs

--- src/main/java/fvarrui/maven/plugin/javapackager/utils/CommandUtils.java
+++ src/main/java/fvarrui/maven/plugin/javapackager/utils/CommandUtils.java
@@ -4,29 +4,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.codehaus.plexus.util.cli.CommandLineException;
 import org.codehaus.plexus.util.cli.Commandline;
 
 public class CommandUtils {
-	
-	private static Object [] expandArray(Object [] array) {
-		List<Object> args = new ArrayList<Object>();
-		for (Object o : array) {
-			if (o.getClass().isArray()) {
-				Object [] a = (Object[]) o;
-				args.addAll(Arrays.asList(a));
-			} else {
-				args.add(o);
-			}
-		}
-		return args.toArray();
-	}
 	
 	private static void createArguments(Commandline command, Object ... arguments) {
 		for (Object argument : arguments) {
@@ -42,13 +27,17 @@ public class CommandUtils {
 	public static String execute(File workingDirectory, String executable, Object ... arguments) throws MojoExecutionException {
 		StringBuffer outputBuffer = new StringBuffer();
 		try { 
-			
-			arguments = expandArray(arguments);
-						
+
 			Commandline command = new Commandline();
 			command.setWorkingDirectory(workingDirectory);
-			command.setExecutable(executable);
-			createArguments(command, arguments);
+
+			if (SystemUtils.IS_OS_WINDOWS) {
+				command.setExecutable(executable);
+				createArguments(command, arguments);
+			} else {
+				command.setExecutable("/bin/bash");
+				createArguments(command, "-c", StringUtils.join(arguments, " "));
+			}
 
 			Logger.info("Executing command: " + StringUtils.join(command.getCommandline(), " "));
 			
@@ -71,42 +60,7 @@ public class CommandUtils {
 		
 		return outputBuffer.toString();
 	}
-	
-	public static String execute2(File workingDirectory, String executable, Object ... arguments) throws MojoExecutionException {
-		StringBuffer outputBuffer = new StringBuffer();
-		try { 
-			
-			arguments = expandArray(arguments);
-						
-			Commandline command = new Commandline();
-			command.setWorkingDirectory(workingDirectory);
-			command.setExecutable(executable);
-			createArguments(command, arguments);
-
-			String line = StringUtils.join(command.getCommandline(), " ");
-			
-			System.out.println(Arrays.asList(command.getCommandline()));
-			
-			Process process = Runtime.getRuntime().exec(line);
-			
-			BufferedReader output = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-			
-			while (process.isAlive()) {
-				if (output.ready()) outputBuffer.append(Logger.info(output.readLine()) + "\n");
-				if (error.ready()) Logger.error(error.readLine());
-			}
-			
-			output.close();
-			error.close();
-			
-		} catch (IOException e) {
-			throw new MojoExecutionException(e.getMessage(), e);
-		}
 		
-		return outputBuffer.toString();
-	}
-	
 	public static String execute(String executable, Object... arguments) throws MojoExecutionException {
 		return execute(new File("."), executable, arguments);
 	}
