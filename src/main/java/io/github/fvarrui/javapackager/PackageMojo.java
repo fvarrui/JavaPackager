8b61a22220c1b9bc8d1e8ded918872f088ce3c1b
fvarrui <4354486+fvarrui@users.noreply.github.com>
2020-05-21 21:12:44 +0100

    U fixs extra parameter default value

--- src/main/java/io/github/fvarrui/javapackager/PackageMojo.java
+++ src/main/java/io/github/fvarrui/javapackager/PackageMojo.java
@@ -218,7 +218,7 @@ public class PackageMojo extends ParentMojo {
 	/**
 	 * Extra properties for customized Velocity templates, accesible through '$this.extra' map. 
 	 */
-	@Parameter(defaultValue = "false", property = "createZipball", required = false)
+	@Parameter(property = "extra", required = false)
 	private Map<String, String> extra;
 
 	public void execute() throws MojoExecutionException {
