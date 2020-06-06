945081a9be359d945419012782a132a7eeebc854
fvarrui <4354486+fvarrui@users.noreply.github.com>
2020-06-06 04:52:51 +0100

    Update CommandUtils.java

--- src/main/java/io/github/fvarrui/javapackager/utils/CommandUtils.java
+++ src/main/java/io/github/fvarrui/javapackager/utils/CommandUtils.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.SystemUtils;
@@ -21,7 +18,7 @@ public class CommandUtils {
 			if (argument == null)
 				continue;
 			if (argument instanceof File)
-				command.createArg().setValue("\"" + ((File) argument).getAbsolutePath() + "\"");
+				command.createArg().setFile((File) argument);
 			else if (argument.getClass().isArray())
 				createArguments(command, argument);
 			else
@@ -42,15 +39,11 @@ public class CommandUtils {
 				command.setExecutable(executable);
 				createArguments(command, arguments);
 			} else {
-				List<Object> argList = new ArrayList<>();
-				argList.add("-c");
-				argList.add(executable);
-				argList.addAll(Arrays.asList(arguments));
 				command.setExecutable("/bin/bash");
-				createArguments(command, argList.toArray(new Object[argList.size()]));
+				createArguments(command, "-c", executable + " " + StringUtils.join(arguments, " "));
 			}
 
-			Logger.info("Executing command: " + Arrays.asList(command.getCommandline()));
+			Logger.info("Executing command: " + StringUtils.join(command.getCommandline(), " "));
 
 			Process process = command.execute();
 
