c4cfd71b4211fba5a25bc37fa0a92a0163432541
fvarrui <4354486+fvarrui@users.noreply.github.com>
2020-11-27 10:26:23 +0000

    Update GenerateDeb.java

--- src/main/java/io/github/fvarrui/javapackager/gradle/GenerateDeb.java
+++ src/main/java/io/github/fvarrui/javapackager/gradle/GenerateDeb.java
@@ -69,7 +69,7 @@ public class GenerateDeb extends ArtifactGenerator {
 		
 		// includes app folder files, except executable file and jre/bin/java
 		debTask.from("build/assets/" + name + ".desktop", c -> {
-			c.into("/opt/" + name);
+			c.into(name);
 		});
 		
 //		// executable
