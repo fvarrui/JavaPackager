12be692d8379c2d1d985810dba6f7eafe421e67b
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2020-12-05 17:45:55 +0000

    Update GenerateDeb.java

--- src/main/java/io/github/fvarrui/javapackager/gradle/GenerateDeb.java
+++ src/main/java/io/github/fvarrui/javapackager/gradle/GenerateDeb.java
@@ -30,48 +30,51 @@ public class GenerateDeb extends ArtifactGenerator {
 			Logger.info(getArtifactName() + " generation skipped by 'linuxConfig.generateDeb' property!");
 			return null;
 		}
-
-		File assetsFolder = linuxPackager.getAssetsFolder();
-		String name = linuxPackager.getName();
-		String description = linuxPackager.getDescription();
-		File appFolder = linuxPackager.getAppFolder();
-		File outputDirectory = linuxPackager.getOutputDirectory();
-		String version = linuxPackager.getVersion();
-		boolean bundleJre = linuxPackager.getBundleJre();
-		String jreDirectoryName = linuxPackager.getJreDirectoryName();
-		File executable = linuxPackager.getExecutable();
-		String organizationName = linuxPackager.getOrganizationName();
-		String organizationEmail = linuxPackager.getOrganizationEmail();
-
-		// generates desktop file from velocity template
-		File desktopFile = new File(assetsFolder, name + ".desktop");
-		VelocityUtils.render("linux/desktop.vtl", desktopFile, linuxPackager);
-		Logger.info("Desktop file rendered in " + desktopFile.getAbsolutePath());
-
-		// generated deb file
-		File debFile = new File(outputDirectory, name + "_" + version + ".deb");
-
-		Deb debTask = createDebTask();
-		debTask.setProperty("archiveFileName", debFile.getName());
-		debTask.setProperty("destinationDirectory", outputDirectory);
-		debTask.setPackageName(name.toLowerCase());
-		debTask.setPackageDescription(description);
-		debTask.setPackager(organizationName);
-		debTask.setUploaders(organizationName);
-		debTask.setMaintainer(organizationName + (organizationEmail != null ? " <" + organizationEmail + ">" : ""));
-		debTask.setPriority("optional");
-		debTask.setArchStr("amd64");
-		debTask.setDistribution("development");
-		debTask.setRelease("1");
 		
-		// installation destination
-		debTask.into("/opt/" + name);
-		
-		// includes app folder files, except executable file and jre/bin/java
-		debTask.from("build/assets/" + name + ".desktop", c -> {
-			c.into(name);
-		});
-		
+		Logger.warn("Sorry! " + getArtifactName() + " generation is not yet available");
+		return null;
+
+//		File assetsFolder = linuxPackager.getAssetsFolder();
+//		String name = linuxPackager.getName();
+//		String description = linuxPackager.getDescription();
+//		File appFolder = linuxPackager.getAppFolder();
+//		File outputDirectory = linuxPackager.getOutputDirectory();
+//		String version = linuxPackager.getVersion();
+//		boolean bundleJre = linuxPackager.getBundleJre();
+//		String jreDirectoryName = linuxPackager.getJreDirectoryName();
+//		File executable = linuxPackager.getExecutable();
+//		String organizationName = linuxPackager.getOrganizationName();
+//		String organizationEmail = linuxPackager.getOrganizationEmail();
+//
+//		// generates desktop file from velocity template
+//		File desktopFile = new File(assetsFolder, name + ".desktop");
+//		VelocityUtils.render("linux/desktop.vtl", desktopFile, linuxPackager);
+//		Logger.info("Desktop file rendered in " + desktopFile.getAbsolutePath());
+//
+//		// generated deb file
+//		File debFile = new File(outputDirectory, name + "_" + version + ".deb");
+//
+//		Deb debTask = createDebTask();
+//		debTask.setProperty("archiveFileName", debFile.getName());
+//		debTask.setProperty("destinationDirectory", outputDirectory);
+//		debTask.setPackageName(name.toLowerCase());
+//		debTask.setPackageDescription(description);
+//		debTask.setPackager(organizationName);
+//		debTask.setUploaders(organizationName);
+//		debTask.setMaintainer(organizationName + (organizationEmail != null ? " <" + organizationEmail + ">" : ""));
+//		debTask.setPriority("optional");
+//		debTask.setArchStr("amd64");
+//		debTask.setDistribution("development");
+//		debTask.setRelease("1");
+//		
+//		// installation destination
+//		debTask.into("/opt/" + name);
+//		
+//		// includes app folder files, except executable file and jre/bin/java
+//		debTask.from("build/assets/" + name + ".desktop", c -> {
+//			c.into(name);
+//		});
+//		
 //		// executable
 //		debTask.from(appFolder.getParentFile(), c -> {
 //			c.include(appFolder.getName() + "/" + executable.getName());
@@ -90,14 +93,14 @@ public class GenerateDeb extends ArtifactGenerator {
 //		debTask.from(desktopFile.getParentFile().getAbsolutePath(), c -> {
 //			c.into("/usr/share/applications");
 //		});
-
-		// symbolic link in /usr/local/bin to app binary
-		debTask.link("/usr/local/bin/" + name, "/opt/" + name + "/" + name, 0777);
-		
-		// runs deb task
-		debTask.getActions().forEach(action -> action.execute(debTask));
-
-		return debFile;
+//
+//		// symbolic link in /usr/local/bin to app binary
+//		debTask.link("/usr/local/bin/" + name, "/opt/" + name + "/" + name, 0777);
+//		
+//		// runs deb task
+//		debTask.getActions().forEach(action -> action.execute(debTask));
+//
+//		return debFile;
 
 	}
 	
