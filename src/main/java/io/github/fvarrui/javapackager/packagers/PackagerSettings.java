d26c5cf5c6ea6ece7aaafcf94419bd2ef015d044
fvarrui <4354486+fvarrui@users.noreply.github.com>
2021-09-06 12:34:47 +0100

    Update PackagerSettings.java

--- src/main/java/io/github/fvarrui/javapackager/packagers/PackagerSettings.java
+++ src/main/java/io/github/fvarrui/javapackager/packagers/PackagerSettings.java
@@ -64,196 +64,389 @@ public class PackagerSettings {
 	protected File packagingJdk;
 	protected Scripts scripts;
 
+	/**
+	 * Get packaging JDK
+	 * @return Packaging JDK
+	 */
 	public File getPackagingJdk() {
 		return packagingJdk;
 	}
 
+	/**
+	 * Get output directory
+	 * @return Output directory
+	 */
 	public File getOutputDirectory() {
 		return outputDirectory;
 	}
 
+	/**
+	 * Get license file 
+	 * @return License file
+	 */
 	public File getLicenseFile() {
 		return licenseFile;
 	}
 
+	/**
+	 * Get icon file
+	 * @return Icon file
+	 */
 	public File getIconFile() {
 		return iconFile;
 	}
 
+	/**
+	 * Get generate installer
+	 * @return Generate installer
+	 */
 	public Boolean getGenerateInstaller() {
 		return generateInstaller;
 	}
 
+	/**
+	 * Get main class
+	 * @return Main class
+	 */
 	public String getMainClass() {
 		return mainClass;
 	}
 
+	/**
+	 * Get name
+	 * @return Name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Get display name
+	 * @return Display name
+	 */
 	public String getDisplayName() {
 		return displayName;
 	}
 
+	/**
+	 * Get version
+	 * @return Version
+	 */
 	public String getVersion() {
 		return version;
 	}
 
+	/**
+	 * Get description
+	 * @return Description
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * Get URL
+	 * @return URL
+	 */
 	public String getUrl() {
 		return url;
 	}
 
+	/**
+	 * Get administrator required
+	 * @return Administrator required
+	 */
 	public Boolean getAdministratorRequired() {
 		return administratorRequired;
 	}
 
+	/**
+	 * Get organization name
+	 * @return Organization name
+	 */
 	public String getOrganizationName() {
 		return organizationName;
 	}
 
+	/**
+	 * Get organization URL
+	 * @return Organization URL
+	 */
 	public String getOrganizationUrl() {
 		return organizationUrl;
 	}
 
+	/**
+	 * Get organization email
+	 * @return Organization email
+	 */
 	public String getOrganizationEmail() {
 		return organizationEmail;
 	}
 
+	/**
+	 * Get bundle JRE
+	 * @return Bundle JRE
+	 */
 	public Boolean getBundleJre() {
 		return bundleJre;
 	}
 
+	/**
+	 * Get customized JRE
+	 * @return Customized JRE
+	 */
 	public Boolean getCustomizedJre() {
 		return customizedJre;
 	}
 
+	/**
+	 * Get JRE path
+	 * @return JRE path
+	 */
 	public File getJrePath() {
 		return jrePath;
 	}
 
+	/**
+	 * Get JDK path
+	 * @return JDK path
+	 */
 	public File getJdkPath() {
 		return jdkPath;
 	}
 
+	/**
+	 * Get additional resourcxes
+	 * @return Additional resources
+	 */
 	public List<File> getAdditionalResources() {
 		return additionalResources;
 	}
 
+	/**
+	 * Get Modules
+	 * @return Modules
+	 */
 	public List<String> getModules() {
 		return modules;
 	}
 
+	/**
+	 * Get additional modules
+	 * @return Additional modules
+	 */
 	public List<String> getAdditionalModules() {
 		return additionalModules;
 	}
 
+	/**
+	 * Get platform
+	 * @return Platform
+	 */
 	public Platform getPlatform() {
 		return platform;
 	}
 
+	/**
+	 * Get env path
+	 * @return Env path
+	 */
 	public String getEnvPath() {
 		return envPath;
 	}
 
+	/**
+	 * Get VM args
+	 * @return VM args
+	 */
 	public List<String> getVmArgs() {
 		return vmArgs;
 	}
 
+	/**
+	 * Get runnable JAR
+	 * @return Runnable JAR
+	 */
 	public File getRunnableJar() {
 		return runnableJar;
 	}
 
+	/**
+	 * Get copy dependencies 
+	 * @return Copy dependencies
+	 */
 	public Boolean getCopyDependencies() {
 		return copyDependencies;
 	}
 
+	/**
+	 * Get JRE directory name
+	 * @return JRE directory name
+	 */
 	public String getJreDirectoryName() {
 		return jreDirectoryName;
 	}
 
+	/**
+	 * Get Windows config
+	 * @return Windows config
+	 */
 	public WindowsConfig getWinConfig() {
 		return winConfig;
 	}
 
+	/**
+	 * Get Linux config
+	 * @return Linux config
+	 */
 	public LinuxConfig getLinuxConfig() {
 		return linuxConfig;
 	}
 
+	/**
+	 * Get Mac OS config
+	 * @return Mac OS config
+	 */
 	public MacConfig getMacConfig() {
 		return macConfig;
 	}
 
+	/** 
+	 * Get create tarball
+	 * @return Create tarball
+	 */
 	public Boolean getCreateTarball() {
 		return createTarball;
 	}
 
+	/**
+	 * Get create zipball
+	 * @return Create zipball
+	 */
 	public Boolean getCreateZipball() {
 		return createZipball;
 	}
 
+	/**
+	 * Get extra parameters
+	 * @return Extra parameters
+	 */
 	public Map<String, String> getExtra() {
 		return extra;
 	}
 
+	/**
+	 * Get if it has to use resources folder as working directory
+	 * @return Use resources folder as working directory
+	 */
 	public boolean isUseResourcesAsWorkingDir() {
 		return useResourcesAsWorkingDir;
 	}
 
+	/**
+	 * Get assets dir
+	 * @return Assets dir
+	 */
 	public File getAssetsDir() {
 		return assetsDir;
 	}
 
+	/**
+	 * Get classpath
+	 * @return Classpath
+	 */
 	public String getClasspath() {
 		return classpath;
 	}
 
+	/**
+	 * Get JRE min version
+	 * @return JRE min version
+	 */
 	public String getJreMinVersion() {
 		return jreMinVersion;
 	}
 
+	/**
+	 * Get Manifest
+	 * @return manifest
+	 */
 	public Manifest getManifest() {
 		return manifest;
 	}
 
+	/**
+	 * Get additional modules paths
+	 * @return Additional module paths
+	 */
 	public List<File> getAdditionalModulePaths() {
 		return additionalModulePaths;
 	}
 
+	/**
+	 * Get file associations
+	 * @return File associations
+	 */
 	public List<FileAssociation> getFileAssociations() {
 		return fileAssociations;
 	}
 
+	/**
+	 * Get scripts
+	 * @return Scripts
+	 */
 	public Scripts getScripts() {
 		return scripts;
 	}
 
 	// fluent api
 
+	/**
+	 * Set output directory
+	 * @param outputDirectory Output directory
+	 * @return Packager settings
+	 */
 	public PackagerSettings outputDirectory(File outputDirectory) {
 		this.outputDirectory = outputDirectory;
 		return this;
 	}
 
+	/**
+	 * Set packaging JDK
+	 * @param packagingJdk Packaging JDK
+	 * @return Packager settings
+	 */
 	public PackagerSettings packagingJdk(File packagingJdk) {
 		this.packagingJdk = packagingJdk;
 		return this;
 	}
 
+	/**
+	 * Set license file
+	 * @param licenseFile License file
+	 * @return Packager settings
+	 */
 	public PackagerSettings licenseFile(File licenseFile) {
 		this.licenseFile = licenseFile;
 		return this;
 	}
 
+	/**
+	 * Set icon file
+	 * @param iconFile Icon file
+	 * @return Packager settings
+	 */
 	public PackagerSettings iconFile(File iconFile) {
 		this.iconFile = iconFile;
 		return this;
 	}
 
+	/**
+	 * Set generate installer
+	 * @param generateInstaller Generate installer
+	 * @return Packager settings
+	 */
 	public PackagerSettings generateInstaller(Boolean generateInstaller) {
 		this.generateInstaller = generateInstaller;
 		return this;
