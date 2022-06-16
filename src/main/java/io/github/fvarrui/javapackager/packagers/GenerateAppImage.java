4b017f132e31064fe60f32c863457ec5a4a77175
fvarrui <4354486+fvarrui@users.noreply.github.com>
2022-06-16 10:18:23 +0100

    Update GenerateAppImage.java

--- src/main/java/io/github/fvarrui/javapackager/packagers/GenerateAppImage.java
+++ src/main/java/io/github/fvarrui/javapackager/packagers/GenerateAppImage.java
@@ -9,6 +9,7 @@ import io.github.fvarrui.javapackager.model.Platform;
 import io.github.fvarrui.javapackager.utils.CommandUtils;
 import io.github.fvarrui.javapackager.utils.FileUtils;
 import io.github.fvarrui.javapackager.utils.Logger;
+import io.github.fvarrui.javapackager.utils.VelocityUtils;
 
 public class GenerateAppImage extends ArtifactGenerator<LinuxPackager> {
 	
@@ -42,27 +43,37 @@ public class GenerateAppImage extends ArtifactGenerator<LinuxPackager> {
 		File outputFolder = packager.getOutputDirectory();
 		String name = packager.getName();
 		File executable = packager.getExecutable();
+		File assetsFolder = packager.getAssetsFolder();
 
 		// output AppImage file
 		File appImage = new File(outputFolder, name + ".AppImage");
 		
-		// AppRun symlink file
-		File appRun = new File(appFolder, "AppRun");
+		// AppDir folder
+		File appDir = new File(assetsFolder, "AppDir");
 		
 		// gets/downloads AppImage tool
 		Logger.info("Getting appimagetool...");
 		File appImageTool = getAppImageTool(packager);
 		Logger.info("App image tool found! " + appImageTool);
+
+		// copies app folder to assets
+		FileUtils.copyFolderContentToFolder(appFolder, appDir);
+				
+		// generates desktop file from velocity template
+		File desktopFile = new File(appDir, name + ".desktop");
+		VelocityUtils.render("linux/desktop.vtl", desktopFile, packager);
+		Logger.info("Desktop file rendered in " + desktopFile.getAbsolutePath());
 		
 		// creates AppRun symlink to startup script
 		Logger.info("Creating AppRun symlink to startup script...");
+		File appRun = new File(appDir, "AppRun");				
 		FileUtils.createSymlink(appRun, new File(executable.getName()));
 		
 		// runs appimagetool on appFolder
 		Logger.info("Running appimagetool on " + appFolder);
 		CommandUtils.execute(
 				appImageTool,
-				appFolder,
+				appDir,
 				appImage
 			);
 		
