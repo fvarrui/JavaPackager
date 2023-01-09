786776f80a450f03a90c067f64653bad5c0dacf8
fvarrui <4354486+fvarrui@users.noreply.github.com>
2023-01-09 00:43:50 +0000

    U several improvements and refactoring

--- src/main/java/io/github/fvarrui/javapackager/utils/CommandUtils.java
+++ src/main/java/io/github/fvarrui/javapackager/utils/CommandUtils.java
@@ -68,5 +68,13 @@ public class CommandUtils {
 			
 		return result;
 	}
+	
+	public static String run(String command) throws IOException {
+    	Process p = Runtime.getRuntime().exec(command);
+        BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String result = br.readLine();	        	
+		br.close();
+		return result;
+	}
 
 }
--- src/main/java/io/github/fvarrui/javapackager/utils/StringUtils.java
+++ src/main/java/io/github/fvarrui/javapackager/utils/StringUtils.java
@@ -1,9 +1,19 @@
 package io.github.fvarrui.javapackager.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringUtils {
-	
+
 	public static String dosToUnix(String input) {
 		return input.replaceAll("\\r\\n", "\n").replaceAll("\\r", "\n");
+	}
+
+	public static String find(String pattern, String data) {
+		Pattern r = Pattern.compile(pattern);
+		Matcher matcher = r.matcher(data);
+		matcher.find();
+		return matcher.group();
 	}
 
 }
