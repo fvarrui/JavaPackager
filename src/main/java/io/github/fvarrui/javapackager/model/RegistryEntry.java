cac6950d3b0b9854e9cadbd8051259f3ca5fe7e7
VIAVANSI\alrodriguez <alrodrru@gmail.com>
2025-02-14 13:28:05 +0100

    New model class for multiple entries

--- src/main/java/io/github/fvarrui/javapackager/model/RegistryEntry.java
+++ src/main/java/io/github/fvarrui/javapackager/model/RegistryEntry.java
@@ -1,9 +1,6 @@
 package io.github.fvarrui.javapackager.model;
 
-
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Windows Registry entry
@@ -16,16 +13,31 @@ public class RegistryEntry implements Serializable {
 	 */
 	private String key;
 
-	private  List<RegistryValue> registryValues = new ArrayList<>();
+	/**
+	 * Windows Registry value name
+	 */
+	private String valueName;
+
+	/**
+	 * Windows Registry value type
+	 */
+	private ValueType valueType = ValueType.REG_SZ;
+
+	/**
+	 * Windows Registry value data
+	 */
+	private String valueData = "";
 
 	public RegistryEntry() {
 		super();
 	}
 
-	public RegistryEntry(String key, List<RegistryValue> registryValues) {
+	public RegistryEntry(String key, String valueName, ValueType valueType, String valueData) {
 		super();
 		this.key = key;
-		this.registryValues = registryValues;
+		this.valueName = valueName;
+		this.valueType = valueType;
+		this.valueData = valueData;
 	}
 
 	public String getKey() {
@@ -36,15 +48,29 @@ public class RegistryEntry implements Serializable {
 		this.key = key;
 	}
 
-	public List<RegistryValue> getRegistryValues() {
-		return registryValues;
+	public String getValueName() {
+		return valueName;
 	}
 
-	public void setRegistryValue(List<RegistryValue> registryValues) {
-		this.registryValues = registryValues;
+	public void setValueName(String valueName) {
+		this.valueName = valueName;
 	}
 
+	public ValueType getValueType() {
+		return valueType;
+	}
 
+	public void setValueType(ValueType valueType) {
+		this.valueType = valueType;
+	}
+
+	public String getValueData() {
+		return valueData;
+	}
+
+	public void setValueData(String valueData) {
+		this.valueData = valueData;
+	}
 
 	public String getRoot() {
 		return key.split(":")[0];
@@ -55,11 +81,43 @@ public class RegistryEntry implements Serializable {
 		return subkey.startsWith("/") ? subkey.substring(1) : subkey;
 	}
 
+	/**
+	 * Returns value type as Inno Setup expects
+	 * https://jrsoftware.org/ishelp/index.php?topic=registrysection
+	 * @return Value type converted to IS format
+	 */
+	public String getValueTypeAsInnoSetupString() {
+		switch(valueType) {
+			case REG_BINARY: return "binary";
+			case REG_DWORD: return "dword";
+			case REG_EXPAND_SZ: return "expandsz";
+			case REG_MULTI_SZ: return "multisz";
+			case REG_QWORD: return "qword";
+			case REG_SZ: return "string";
+			default: return "none";
+		}
+	}
 
+	/**
+	 * Returns value type as WIX Toolset expects
+	 * https://wixtoolset.org/documentation/manual/v3/xsd/wix/registryvalue.html
+	 */
+	public String getValueTypeAsWIXToolsetString() {
+		switch(valueType) {
+			case REG_BINARY: return "binary";
+			case REG_DWORD: return "integer";
+			case REG_EXPAND_SZ: return "expandable";
+			case REG_MULTI_SZ: return "multiString";
+			case REG_QWORD: return "integer";
+			case REG_SZ: return "string";
+			default: return "none";
+		}
+	}
 
 	@Override
 	public String toString() {
-		return "RegistryEntry [key=" + key + ", registryValues=" + registryValues + "]";
+		return "RegistryEntry [key=" + key + ", valueName=" + valueName + ", valueType=" + valueType + ", valueData="
+				+ valueData + "]";
 	}
 
 }
